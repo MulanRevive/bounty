e19f44b0d7be6f144d0de32c3a2f768756eeddf2
nobodxbodon <nobodxbodon.github@gmail.com>
2020-01-29 23:21:44 -0800

    自动测试

---
+++ 测试代码/README.md
@@ -0,0 +1,15 @@
+
+### 运行测试
+
+`> python 测试.py`，应该所有测试都通过。
+
+#### 其他
+
+可以用--interact在运行代码后进入交互环境调试
+```
+>..\原始资料\可执行文件\ulang-0.2.2.exe --interact while.ul
+Welcome to ulang's REPL..
+Type 'help' for more informations.
+> sum
+10
+```
---
+++ 测试代码/测试.py
@@ -0,0 +1,11 @@
+import subprocess
+
+期望值 = {"while.ul": b'10'}
+
+for 文件 in 期望值:
+    进程 = subprocess.Popen(["..\\原始资料\\可执行文件\\ulang-0.2.2.exe", 文件], stdout=subprocess.PIPE)
+    输出 = 进程.communicate()[0]
+    if 输出 == 期望值[文件]:
+        print("通过： " + 文件)
+    else:
+        print("失败： " + 文件 + "期望：" + 期望值[文件] + " 实际：" + 输出)
