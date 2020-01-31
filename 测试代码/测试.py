e8603da9bba70aef3b8be83b075bd3eef46371e2
nobodxbodon <nobodxbodon.github@gmail.com>
2020-01-31 13:43:50 -0800

    break, continue测试

--- 测试代码/测试.py
+++ 测试代码/测试.py
@@ -2,6 +2,8 @@ import subprocess
 
 # 不确定为何输出是bytes：https://stackoverflow.com/questions/6269765/what-does-the-b-character-do-in-front-of-a-string-literal
 期望值 = {
+    "break.mulan": b'012',
+    "continue.mulan": b'01245',
     "range.mulan": b'012123-113',
     "for_colon.mulan": b'6',
     "for_in.mulan": b'10',
